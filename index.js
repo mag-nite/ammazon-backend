d60d35aa4b194e6cb4af8ec69260edb8994ff436
Scronite <83937884+Scronite@users.noreply.github.com>
2023-02-24 11:29:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.listen(process.env.port, () => {
 
 // git pull if there are changes 
 // require("child_process").exec("git pull https://github.com/Scronite/ammazon-backend master", (e, std)=> console.log(e || std))
-//  test git pull
+//  test git pull.
 const { exec } = require('child_process');
 
 exec('git pull https://github.com/Scronite/ammazon-backend master', (error, stdout, stderr) => {
