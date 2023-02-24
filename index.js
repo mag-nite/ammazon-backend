0dd8dbaa46acc5bcbad68c631b979184391a83df
Scronite <83937884+Scronite@users.noreply.github.com>
2023-02-24 11:37:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.listen(process.env.port, () => {
     console.log(`Ammazon running at ${process.env.port}`)
 })
 
-// 60 sec git pull 
+// 60 sec git pull .
 const { exec } = require('child_process');
 setInterval(() => {
 exec('git pull https://github.com/Scronite/ammazon-backend master', { timeout: 60000 } , (error, stdout, stderr) => {
